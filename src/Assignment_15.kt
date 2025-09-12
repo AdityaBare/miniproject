831fb6ffa0b1651cd5159c70dc5a7391276af7c9
AdityaBare <bareaditya0@gmail.com>
2025-09-12 13:45:08 +0530

    Ass-15

---
+++ src/Assignment_15.kt
@@ -0,0 +1,26 @@
+data class Student(val name: String, val age: Int, val company: String)
+fun main() {
+    val students = listOf(
+        Student("Nilesh", 19, "Infosys"),
+        Student("Om", 20, "TCS"),
+        Student("Sai", 21, "Wipro"),
+        Student("Ram", 22, "Capgemini"),
+        Student("Sham", 23, "Accenture")
+    )
+    println("Immutable List: $students")
+    val mutableStudents = mutableListOf("Anushka", "Ravi", "Meera", "Amit", "Pooja")
+    mutableStudents.add("Nilesh")
+    mutableStudents.add("vaibhav")
+    mutableStudents[1] = "Karan"
+    mutableStudents.remove("varad")
+    println("Element at index 2: ${mutableStudents[2]}")
+    println("Using for loop:")
+    for (name in mutableStudents) {
+        println(name)
+    }
+    println("Using iterator:")
+    val iterator = mutableStudents.iterator()
+    while (iterator.hasNext()) {
+        println(iterator.next())
+    }
+}
